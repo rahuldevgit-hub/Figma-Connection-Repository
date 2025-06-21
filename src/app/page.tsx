7d5c4c345909da1aa59c1d6c5acd9ff767af55b3
Gajanand <gajanand@doomshell.com>
2025-06-21 13:09:34 +0530

    changes in home page file to check docker deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docsss
+            Read our doc testttt
           </a>
         </div>
       </main>
