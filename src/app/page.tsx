0311851d6520f4ee0fb02821294bc90261df5b72
Gajanand <gajanand@doomshell.com>
2025-06-21 13:38:03 +0530

    added home page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our doc testttt
+            Read our docs
           </a>
         </div>
       </main>
