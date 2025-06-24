e551bbba098e2c22a591b04bf590a27da100c4dd
Gajanand <gajanand@doomshell.com>
2025-06-24 15:51:31 +0530

    deployment test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs test
+            Read our docs testing
           </a>
         </div>
       </main>
